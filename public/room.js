653441f343b0e14cb04a1ef2a61b6cdbae0fe485
“Sristi <24sristi@gmail.com>
2021-07-09 03:46:11 +0530

    UI changes

--- public/room.js
+++ public/room.js
@@ -5,6 +5,8 @@ myVideo.muted = true;
 let Chat_button = document.querySelector('.main_chat_button')
 let Chat_window = document.querySelector('.main_right')
 let Video_window = document.querySelector('.main_left')
+let main_screen_video = document.querySelector('.main_screen_video video');
+let main_controls = document.querySelector('.main_controls')
 
 const peer = new Peer(undefined, {
   path: '/peerjs',
@@ -18,19 +20,34 @@ navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
-  myVideoStream = stream;
+    myVideoStream = stream;
+    main_screen_video.srcObject = stream;
+    main_screen_video.addEventListener('loadedmetadata', () => {
+    main_screen_video.play();
+  })
   addVideoStream(myVideo, stream);
+  myVideo.addEventListener("click",function(){
+    main_screen_video.srcObject = stream;
+  
+})
 
   peer.on('call', call => {
     call.answer(stream)
     const video = document.createElement('video')
+    video.classList.add("otherVideo")
+    
     call.on('stream', userVideoStream => {
       addVideoStream(video, userVideoStream);
+      video.addEventListener("click",function(){
+          main_screen_video.srcObject = userVideoStream;
+        
+      })
+      
     });
   }, err => { console.log(err); })
 
 
-  socket.on('user-connected', (userId,userName) => {
+  socket.on('user-connected', (userId, userName) => {
     setTimeout(() => {
       //user joined
       connectToNewUser(userId, stream);
@@ -39,18 +56,20 @@ navigator.mediaDevices.getUserMedia({
   })
 });
 
-socket.on('user-disconnected', (userId,userName) => {
+socket.on('user-disconnected', (userId, userName) => {
   if (peer[userId]) peer[userId].close()
   $('.messages').append(`<li class="message">${userName} left the meeting</li>`);
 })
 
 peer.on('open', id => {
-  socket.emit('join-room', ROOM_ID, id,name);
+  socket.emit('join-room', ROOM_ID, id, name);
 })
 
 const connectToNewUser = (userId, stream) => {
   const call = peer.call(userId, stream)
+
   const video = document.createElement('video')
+  
   call.on('stream', userVideoStream => {
     addVideoStream(video, userVideoStream)
   })
@@ -71,15 +90,15 @@ const addVideoStream = (video, stream) => {
 
 let msg = $('input')
 
-$('html').keydown((e) =>{
+$('html').keydown((e) => {
   if (e.which == 13 && msg.val().length !== 0) {
     console.log(msg.val())
-    socket.emit('message', msg.val(),name);
+    socket.emit('message', msg.val(), name);
     msg.val('')
   }
 })
 
-socket.on('createMessage',(message,userName) => {
+socket.on('createMessage', (message, userName) => {
   $('.messages').append(`<li class="message"><b>${userName}</b><br/>${message}</li>`);
   scrollbottom()
 })
@@ -105,7 +124,6 @@ const muteUnmute = () => {
 const setMuteButton = () => {
   const html = `
     <i class="fas fa-microphone"></i>
-    <span>Mute</span>
   `
   document.querySelector('.main_mute_button').innerHTML = html;
 }
@@ -113,7 +131,6 @@ const setMuteButton = () => {
 const setUnmuteButton = () => {
   const html = `
     <i class="unmute fas fa-microphone-slash"></i>
-    <span>Unmute</span>
   `
   document.querySelector('.main_mute_button').innerHTML = html;
 }
@@ -132,7 +149,6 @@ const playStop = () => {
 const setStopVideo = () => {
   const html = `
     <i class="fas fa-video"></i>
-    <span>Stop Video</span>
   `
   document.querySelector('.main_video_button').innerHTML = html;
 }
@@ -140,22 +156,21 @@ const setStopVideo = () => {
 const setPlayVideo = () => {
   const html = `
   <i class="stop fas fa-video-slash"></i>
-    <span>Play Video</span>
   `
   document.querySelector('.main_video_button').innerHTML = html;
 }
 
-Chat_button.addEventListener("click",function(){
-  if(Chat_window.classList.contains("selected"))
-  {
+Chat_button.addEventListener("click", function () {
+  if (Chat_window.classList.contains("selected")) {
     Chat_window.classList.remove("selected")
-    Chat_window.style.display="none"
+    Chat_window.style.display = "none"
     Video_window.style.flex = "1"
+    main_controls.style.width ="83%"
   }
-  else
-  {
+  else {
     Chat_window.classList.add("selected")
-    Chat_window.style.display="flex"
+    Chat_window.style.display = "flex"
     Video_window.style.flex = "0.8"
+    main_controls.style.width ="65%"
   }
 })
